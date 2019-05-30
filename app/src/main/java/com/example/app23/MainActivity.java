95bc1ae8e8d6d1c6b3d4c4dd055441eab8563804
Harold <harold.malecot@gmail.com>
2019-05-30 13:53:41 +0200

    vertical & horizontal ok adapté à la taille de l'écran ok
    next step : 5 layout
    écouter : podcast & artistes
    lire : articles
    danser : events & BP

--- app/src/main/java/com/example/app23/MainActivity.java
+++ app/src/main/java/com/example/app23/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,17 +19,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //2 - Inflate the menu and add it to the Toolbar
-        // getMenuInflater().inflate(R.menu.menu_activity_main, menu);
         getMenuInflater().inflate(R.menu.menu_activity_main, menu);
         return true;
     }
-
-    // ----
 
     private void configureToolbar(){
         // Get the toolbar view inside the activity layout
         Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
         // Sets the Toolbar
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_activity_main_params:
+                Toast.makeText(this, "Il n'y a rien à paramétrer ici, passez votre chemin...", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_activity_main_search:
+                Toast.makeText(this, "Recherche indisponible, demandez plutôt l'avis de Google, c'est mieux et plus rapide.", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
