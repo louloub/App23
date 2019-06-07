6f19cb7dc5c1afba1e43bb1dde0642e9e8f2ad3c
Harold <harold.malecot@gmail.com>
2019-06-07 16:40:42 +0200

    affichage des infos du JSON mais en vrac

--- app/src/main/java/com/example/app23/ArtistesActivity.java
+++ app/src/main/java/com/example/app23/ArtistesActivity.java
@@ -59,6 +59,7 @@ public class ArtistesActivity extends AppCompatActivity
         mContext = getApplicationContext();
 
         textView = findViewById(R.id.textView);
+        textView.setText("test");
 
         loadArtistes2();
     }
@@ -124,14 +125,21 @@ public class ArtistesActivity extends AppCompatActivity
         RequestQueue requestQueue = Volley.newRequestQueue(mContext);
 
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
-                (Request.Method.GET, URL_API, null, response -> {
-                    try {
+                (Request.Method.GET, URL_API, null, response ->
+
+        {
+            textView.setText(response.toString());
+
+            try
+                    {
+                        Log.d(TAG,"String loadArtistes ");
+
                         //converting the string to json array object
                         JSONArray array = new JSONArray(response);
 
                         //traversing through all the object
-                        for (int i = 0; i < array.length(); i++) {
-
+                        for (int i = 0; i < array.length(); i++)
+                        {
                             Log.d(TAG,"String loadArtistes = " + i);
 
                             //getting product object from json array
@@ -146,7 +154,6 @@ public class ArtistesActivity extends AppCompatActivity
                             recyclerView.setAdapter(adapter);
 
                         }
-
 
                     } catch (JSONException e) {
                         e.printStackTrace();
