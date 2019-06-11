bf5c70298a5d4fddae58fd931c4a4a7bde9bfa17
Harold <harold.malecot@gmail.com>
2019-06-11 18:28:43 +0200

    page artiste s'affiche bien, mais il faut la custom le contenu n'est pas encore celui envoyé dans l'intent

--- app/src/main/java/com/example/app23/ArtistesPage.java
+++ app/src/main/java/com/example/app23/ArtistesPage.java
@@ -2,15 +2,22 @@ package com.example.app23;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ArtistesPage extends AppCompatActivity {
+
+    private static final String LOG = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artistes_page);
 
-        Bundle bundle = getIntent().getExtras();
+        Artistes artiste = (Artistes) getIntent().getSerializableExtra("artiste");
+
+        String firstName = artiste.getFirstname();
+        String lastName = artiste.getLastname();
+        String age = artiste.getAge();
 
     }
 }
