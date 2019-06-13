919b129014cb31d64d3a061b0a3b7a2e7daab398
Harold <harold.malecot@gmail.com>
2019-06-13 14:59:46 +0200

    player Soundcloud ok plus de crash et plus de message pour lire hors de soundcloud

--- app/src/main/java/com/example/app23/PodcastActivity.java
+++ app/src/main/java/com/example/app23/PodcastActivity.java
@@ -1,5 +1,8 @@
 package com.example.app23;
 
+import android.content.Intent;
+import android.net.MailTo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,11 +33,7 @@ public class PodcastActivity extends AppCompatActivity {
         // INTEGRATION SOUNDCLOUD IFRAME
         //------------------------------
 
-        // <iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/148369466&color=%23a1c332&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
-        // <iframe allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/148369466&color=%23a1c332&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
-        // initial-scale="1.0" maximum-scale="1.0" user-scalable="no"
-
-        String streamUrl = "https%3A//api.soundcloud.com/playlists/148369466";
+        String streamUrl = "https%3A//api.soundcloud.com/playlists/148369466&color=%23a1c332";
         // String streamUrl = "https://soundcloud.com/yourdjmusic/sets/yourdj-podcast";
         String iframe = "<!DOCTYPE html>" +
                 "<html>" +
@@ -56,50 +55,66 @@ public class PodcastActivity extends AppCompatActivity {
                 super.onHideCustomView();
             }
         });
-
         webView.setNetworkAvailable(true);
         webView.setVisibility(View.VISIBLE);
-        webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
-
-        webView.clearView();
         webView.requestLayout();
-
-        // webView.measure(1000, 1000);
-
-        // webView.getSettings().setBuiltInZoomControls(true);
-
-        // webView.getSettings().setDisplayZoomControls(true);
-
-        // webView.setInitialScale(1);
-
         webView.getSettings().setUserAgentString("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36");
         webView.loadDataWithBaseURL("", iframe, "text/html", "UTF-8", "");
     }
 
-    // color=%23a1c332&amp
+    //------------------------------
+    // INTEGRATION SOUNDCLOUD IFRAME
+    //------------------------------
+
+    /*<iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay"
+    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/148369466&color=%23a1c332&auto_play=false&"
+            "hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"></iframe>*/
+
+    /*&amp;auto_play=false&amp; hide_related=false&amp; " +
+            "show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;*/
 
     public void loadSoundCloudWebView()
     {
-        String streamUrl = "https%3A//api.soundcloud.com/playlists/148369466";
-        // String streamUrl = "https://soundcloud.com/yourdjmusic/sets/yourdj-podcast";
+        String streamUrl = "https%3A//api.soundcloud.com/playlists/148369466&color=%23a1c332&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false";
+        //String streamUrl = "https://soundcloud.com/yourdjmusic/sets/yourdj-podcast&color=%23a1c332";
         String iframe = "<!DOCTYPE html>" +
                 "<html>" +
-                "<body>" +
-                "<iframe width=\"100%\" height=\"1000\" id=\"sc-widget\" scrolling=\"yes\" frameborder=\"yes\" " +
-                "src=\"https://w.soundcloud.com/player/?url="+streamUrl+"&amp;auto_play=false&amp; hide_related=false&amp; show_comments=true&amp; show_user=true&amp;  ;show_reposts=false&amp; visual=true\">" +
-                "</iframe>" +
-                "</body>" +
+                    "<body>" +
+                        "<iframe width=\"100%\" height=\"1000\" id=\"sc-widget\" scrolling=\"yes\" frameborder=\"yes\" " +
+                            "src=\"https://w.soundcloud.com/player/?url="+streamUrl+"\">" +
+                        "</iframe>" +
+                    "</body>" +
                 "</html>";
-        WebView webView = (WebView) findViewById(R.id.WebViewSoundCloud);
+        // WebView webView = (WebView) findViewById(R.id.WebViewSoundCloud);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.addJavascriptInterface(new WebViewResizer(), "WebViewResizer");
         webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return loadUrl(view, url);
+            }
+
             @Override
             public void onPageFinished(WebView webView, String url) {
-                webView.loadUrl("javascript:window.WebViewResizer.processHeight(document.querySelector('body').offsetHeight);");
+                // webView.loadUrl("javascript:window.WebViewResizer.processHeight(document.querySelector('body').offsetHeight);");
                 super.onPageFinished(webView, url);
+            }
+
+            private boolean loadUrl(WebView view, String url) {
+                if (url.startsWith("http:") || url.startsWith("https:")) {
+                    view.loadUrl(url);
+                    return false;
+                }
+                // Otherwise allow the OS to handle it
+                else if (url.startsWith("tel:")) {
+                    Intent tel = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
+                    startActivity(tel);
+                    return true;
+                }
+                return true;
             }
         });
         webView.loadDataWithBaseURL("", iframe, "text/html", "UTF-8", "");
