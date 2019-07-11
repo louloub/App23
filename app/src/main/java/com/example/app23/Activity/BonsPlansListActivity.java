fbe9c488abbca0432cf2f9585e279e03d854d6f7
Harold <harold.malecot@gmail.com>
2019-07-11 18:02:57 +0200

    Taf effectué : Alerte Dialog avec Newsletter si pas de bons plans
    TO DO :
    Pimper l'Alerte Dialog ?

--- app/src/main/java/com/example/app23/Activity/BonsPlansListActivity.java
+++ app/src/main/java/com/example/app23/Activity/BonsPlansListActivity.java
@@ -2,11 +2,16 @@ package com.example.app23.Activity;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -273,7 +278,6 @@ public class BonsPlansListActivity extends OptionMenuActivity {
                             // Do something when error occurred
                         }
                 );
-
         requestQueue.add(jsonArrayEventRequest);
     }
 
@@ -282,27 +286,49 @@ public class BonsPlansListActivity extends OptionMenuActivity {
         // Setup Alert builder
         android.support.v7.app.AlertDialog.Builder myPopup = new android.support.v7.app.AlertDialog.Builder(this);
         myPopup.setTitle("Pas encore de bons plans disponibles");
-        myPopup.setMessage("Tu peux t'inscrire à notre newsletter ou nous suivre sur Facebook pour ne rien rater :");
+        myPopup.setMessage("Tu peux t'inscrire à notre newsletter pour être prévenu en live juste ici :");
 
-        /*// Ddd a radio button list
-        String[] villes = {"Montpellier", "Toulouse", "Marseille", "Bordeaux", "Nantes"};
-        int checkedItems = 0;
-        myPopup.setSingleChoiceItems(villes, checkedItems, (dialog, which) -> {
-        });
-        myPopup.setPositiveButton("Valider", (dialogInterface, i) -> {
-            ListView lw = ((android.support.v7.app.AlertDialog)dialogInterface).getListView();
-            Object checkedItem = lw.getAdapter().getItem(lw.getCheckedItemPosition());
-            Toast.makeText(getApplicationContext(), "Tu as choisi " + checkedItem, Toast.LENGTH_LONG).show();
-            // TODO : utiliser "checkedItem" pour le choix du contenu par ville
-        });
+        /*String iframe = "<!DOCTYPE html>" +
+                "<html>" +
+                "<body>" +
+                "<iframe width=\"100%\" height=\"1000\" id=\"sc-widget\" scrolling=\"yes\" frameborder=\"yes\" " +
+                "src=\"" + codeHtmlNewsletter + "\">" +
+                "</iframe>" +
+                "</body>" +
+                "</html>";*/
 
-        *//*myPopup.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+        WebView webViewNewsletter = new WebView(getApplicationContext());
+        webViewNewsletter.getSettings().setJavaScriptEnabled(true);
+        webViewNewsletter.addJavascriptInterface(new WebViewResizer(), "WebViewResizer");
+        webViewNewsletter.setWebViewClient(new WebViewClient() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(getApplicationContext(), "Vous avez cliquez sur Non", Toast.LENGTH_SHORT).show();
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
             }
-        });*/
 
+            @Override
+            public void onPageFinished(WebView webView, String url) {
+                // webView.loadUrl("javascript:window.WebViewResizer.processHeight(document.querySelector('body').offsetHeight);");
+                super.onPageFinished(webView, url);
+            }
+
+            private boolean loadUrl(WebView view, String url) {
+                if (url.startsWith("http:") || url.startsWith("https:")) {
+                    view.loadUrl(url);
+                    return false;
+                }
+                // Otherwise allow the OS to handle it
+                else if (url.startsWith("tel:")) {
+                    Intent tel = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
+                    // startActivity(tel);
+                    return true;
+                }
+                return true;
+            }
+        });
+        webViewNewsletter.loadDataWithBaseURL("", codeHtmlNewsletter, "text/html", "UTF-8", "");
+        myPopup.setView(webViewNewsletter);
         myPopup.setCancelable(false);
         myPopup.setNegativeButton("Fermer", new DialogInterface.OnClickListener() {
             @Override
@@ -315,4 +341,93 @@ public class BonsPlansListActivity extends OptionMenuActivity {
         android.support.v7.app.AlertDialog dialog = myPopup.create();
         myPopup.show();
     }
+
+    private class WebViewResizer {
+        @JavascriptInterface
+        public void processHeight(String height) {
+            // height is in DP units. Convert it to PX if you are adjusting the WebView's height.
+            // height could be 0 if WebView visibility is Visibility.GONE.
+            // If changing the WebView height, do it on the main thread!
+        }
+    }
+
+
+    String codeHtmlNewsletter =
+            "<!-- Begin Mailchimp Signup Form -->\n" +
+                    "<link href=\"//cdn-images.mailchimp.com/embedcode/classic-10_7.css\" rel=\"stylesheet\" type=\"text/css\" />\n" +
+                    "<style type=\"text/css\">\n" +
+                    "#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; border:1px}\n" +
+                    "ul#ul li {\n" +
+                    "    list-style-type: none;\n" +
+                    "\n" +
+                    "   }\n" +
+                    "\n" +
+                    "\t/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.\n" +
+                    "\t   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file.\n" +
+                    "\t   list-style-position: outside;\n" +
+                    "\n" +
+                    "   display: inline;\n" +
+                    "   padding: 2px ;\n" +
+                    "   margin: 0;\n" +
+                    "   width: 19%; */\n" +
+                    "</style>\n" +
+                    "<div id=\"mc_embed_signup\">\n" +
+                    "<form action=\"https://yourdj.us5.list-manage.com/subscribe/post?u=d0b2aaee0796a328290834c4a&amp;id=445e00e010\" class=\"validate\" id=\"mc-embedded-subscribe-form\" method=\"post\" name=\"mc-embedded-subscribe-form\" novalidate=\"\" target=\"_blank\">\n" +
+                    "<div id=\"mc_embed_signup_scroll\">\n" +
+                    "<h2>Newsletter Hebdomadaire YourDJ</h2>\n" +
+                    "\n" +
+                    "<div class=\"mc-field-group\"><label for=\"mce-EMAIL\">Adresse Email <span class=\"asterisk\">*</span> </label> <input class=\"required email\" id=\"mce-EMAIL\" name=\"EMAIL\" type=\"email\" value=\"\" /></div>\n" +
+                    "\n" +
+                    "<div class=\"mc-field-group\"><label for=\"mce-FNAME\">Prénom </label> <input id=\"mce-FNAME\" name=\"FNAME\" type=\"text\" value=\"\" /></div>\n" +
+                    "\n" +
+                    "<div class=\"mc-field-group\"><label for=\"mce-LNAME\">Nom </label> <input id=\"mce-LNAME\" name=\"LNAME\" type=\"text\" value=\"\" /></div>\n" +
+                    "\n" +
+                    "<div class=\"mc-field-group input-group\"><strong>Pour quelle ville veux tu recevoir des bons plans ? </strong><br /></div>\n" +
+                    "&nbsp;\n" +
+                    "<ul id=\"ul\">\n" +
+                    "\t<li><input name=\"group[18949][1]\" type=\"checkbox\" value=\"1\" /><label for=\"mce-group[18949]-18949-0\"> &nbsp;Montpellier</label></li>\n" +
+                    "\t<li><input name=\"group[18949][2]\" type=\"checkbox\" value=\"2\" /><label for=\"mce-group[18949]-18949-1\"> &nbsp;Toulouse</label></li>\n" +
+                    "\t<li><input name=\"group[18949][4]\" type=\"checkbox\" value=\"4\" /><label for=\"mce-group[18949]-18949-2\"> &nbsp;Marseille</label></li>\n" +
+                    "\t<li><input name=\"group[18949][8]\" type=\"checkbox\" value=\"8\" /><label for=\"mce-group[18949]-18949-3\"> &nbsp;Bordeaux</label></li>\n" +
+                    "\t<li><input name=\"group[18949][16]\" type=\"checkbox\" value=\"16\" /><label for=\"mce-group[18949]-18949-4\">&nbsp;Nantes</label></li>\n" +
+                    "</ul>\n" +
+                    "\n" +
+                    "\n" +
+                    "<div class=\"clear\" id=\"mce-responses\">\n" +
+                    "<div class=\"response\" id=\"mce-error-response\" style=\"display:none\">&nbsp;</div>\n" +
+                    "\n" +
+                    "<div class=\"response\" id=\"mce-success-response\" style=\"display:none\">&nbsp;</div>\n" +
+                    "</div>\n" +
+                    "<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->\n" +
+                    "\n" +
+                    "<div aria-hidden=\"true\" style=\"position: absolute; left: -5000px;\"><input name=\"b_d0b2aaee0796a328290834c4a_445e00e010\" tabindex=\"-1\" type=\"text\" value=\"\" /></div>\n" +
+                    "\n" +
+                    "<div class=\"clear\"><input class=\"button\" id=\"mc-embedded-subscribe\" name=\"subscribe\" type=\"submit\" value=\"S'inscrire\" /></div>\n" +
+                    "</div>\n" +
+                    "</form>\n" +
+                    "</div>\n" +
+                    "<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text'; /*\n" +
+                    " * Translated default messages for the $ validation plugin.\n" +
+                    " * Locale: FR\n" +
+                    " */\n" +
+                    "$.extend($.validator.messages, {\n" +
+                    "        required: \"Ce champ est requis.\",\n" +
+                    "        remote: \"Veuillez remplir ce champ pour continuer.\",\n" +
+                    "        email: \"Veuillez entrer une adresse email valide.\",\n" +
+                    "        url: \"Veuillez entrer une URL valide.\",\n" +
+                    "        date: \"Veuillez entrer une date valide.\",\n" +
+                    "        dateISO: \"Veuillez entrer une date valide (ISO).\",\n" +
+                    "        number: \"Veuillez entrer un nombre valide.\",\n" +
+                    "        digits: \"Veuillez entrer (seulement) une valeur numérique.\",\n" +
+                    "        creditcard: \"Veuillez entrer un numéro de carte de crédit valide.\",\n" +
+                    "        equalTo: \"Veuillez entrer une nouvelle fois la même valeur.\",\n" +
+                    "        accept: \"Veuillez entrer une valeur avec une extension valide.\",\n" +
+                    "        maxlength: $.validator.format(\"Veuillez ne pas entrer plus de {0} caractères.\"),\n" +
+                    "        minlength: $.validator.format(\"Veuillez entrer au moins {0} caractères.\"),\n" +
+                    "        rangelength: $.validator.format(\"Veuillez entrer entre {0} et {1} caractères.\"),\n" +
+                    "        range: $.validator.format(\"Veuillez entrer une valeur entre {0} et {1}.\"),\n" +
+                    "        max: $.validator.format(\"Veuillez entrer une valeur inférieure ou égale à {0}.\"),\n" +
+                    "        min: $.validator.format(\"Veuillez entrer une valeur supérieure ou égale à {0}.\")\n" +
+                    "});}(jQuery));var $mcj = jQuery.noConflict(true);</script><!--End mc_embed_signup-->";
+
 }
